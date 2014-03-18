c04198d8b0660e39f4fc6fc5715e4996eeeb45f1
Nathan Jordan <natedagreat27274@gmail.com>
2014-03-17 19:09:42 -0700

    some neuron_group processing

--- ncsdaemon/simhelper.py
+++ ncsdaemon/simhelper.py
@@ -7,7 +7,7 @@ import json
 from threading import Thread
 from ncsdaemon.util import WriteRedirect
 from ncsdaemon.util import SchemaLoader
-import jsonschema.validate
+from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
 SIM_DATA_DIRECTORY = '/var/ncs/sims/'
@@ -96,6 +96,8 @@ class SimHelper(SimHelperBase):
                 "message": "Failed to initialize simulation"
             }
             return info
+        # after the init, we can add stims and reports
+        errors += ModelHelper.add_stims_and_reports(self.simulation, model)
         # generate a new ID for the ism
         sim_id = Crypt.generate_sim_id()
         #create the directory for sim information like reports
@@ -149,53 +151,104 @@ class SimHelper(SimHelperBase):
 class ModelHelper(object):
 
     @classmethod
-    def process_model(cls, sim, model):
+    def process_model(cls, sim, entity_dicts):
         # create a list of errors to output if neccessary
         errors = []
         # schemaloader
         schema_loader = SchemaLoader()
         # see if the schema works
         try:
-            jsonschema.validate(model,
-                                schema_loader.get_schema('transfer_schema'))
+            validate(entity_dicts, schema_loader.get_schema('transfer_schema'))
         # if it doesn't pass validation, return a bad request error
         except ValidationError:
             error = "Improper json format"
-        sim_model = model['model']
-        neurons = model['neurons']
-        synapses = model['synapses']
-        stimuli = model['stimuli']
-        reports = model['reports']
-        groups = model['groups']
-        model = model['model']
+        # get the lists of entities
+        top_group_id = entity_dicts['top_group']
+        neurons = entity_dicts['neurons']
+        synapses = entity_dicts['synapses']
+        groups = entity_dicts['groups']
         # add neurons to sim
         for neuron in neurons:
             # TODO: Validate neuron spec
             pass
             # Get neuron type from the model
-            neuron_type = neuron['specification']['type']
+            neuron_type = neuron['specification']['neuron_type']
+            neuron_type = ModelHelper.convert_neuron_type(neuron_type)
+            spec = neuron['specification']
+            ModelHelper.process_normal_uniform_parameters(spec)
+            ModelHelper.convert_unicode_ascii(spec)
+            del spec['neuron_type']
             sim.addNeuron(neuron['_id'], neuron_type, spec)
         for synapse in synapses:
             # TODO: Validate synapse spec
             pass
             # Get synapse type from the model
-            synapse_type = synapse['specification']['type']
+            synapse_type = synapse['specification']['synapse_type']
+            spec = synapse['specification']
             sim.addSynapse(synapse['_id'], synapse_type, spec)
+        for group in groups:
+            if group['_id'] == top_group_id:
+                top_group = group
+        ModelHelper.traverse_groups(top_group,
+                                    entity_dicts,
+                                    top_group['entity_name'])
+        print "lolz"
+        return errors
+
+    @classmethod
+    def traverse_groups(cls, group, entity_dicts, location_string):
+        spec = group['specification']
+        new_neuron_group = {}
+        # if the neuron_groups key doesn't exist in entity_dicts
+        if 'neuron_groups' not in entity_dicts:
+            # make it an empty list
+            entity_dicts['neuron_groups'] = []
+        # find the neuron that corresponds to the id in the neuron_group
+        for neuron_group in spec['neuron_groups']:
+            # set the parameters
+            new_neuron_group['label'] = neuron_group['label']
+            new_neuron_group['count'] = neuron_group['count']
+            new_neuron_group['geometry'] = neuron_group['geometry']
+            # TODO This will eventually need to be absolute geometry
+            new_neuron_group['location'] = neuron_group['location']
+            # this is to identify stuff for connections and reports
+            new_neuron_group['location_string'] = location_string + ':' + \
+                neuron_group['label']
+            neuron_id = neuron_group['neuron']
+            for neuron in entity_dicts['neurons']:
+                if neuron['_id'] == neuron_id:
+                    new_neuron_group['neuron'] = neuron
+            entity_dicts['neuron_groups'].append(new_neuron_group)
+        # call this method for each subgroup
+        for subgroup in spec['subgroups']:
+            for group in entity_dicts['groups']:
+                if subgroup['group'] == group['_id']:
+                    ModelHelper.traverse_groups(group,
+                                                entity_dicts,
+                                                location_string + ':' +
+                                                group['entity_name'])
+
+
+    def add_stims_and_reports(cls, sim, entity_dicts):
+        errors = []
+        stimuli = entity_dicts['stimuli']
+        reports = entity_dicts['reports']
         for stimulus in stimuli:
             # TODO: Validate stimulus spec
             pass
             # Get stimulus type from the model
-            stimulus_type = stimulus['specification']['type']
+            stimulus_type = stimulus['specification']['stimulus_type']
+            spec = stimulus['specification']
             prob = stimulus['specification']['probability']
             time_start = stimulus['specification']['time_start']
             time_end = stimulus['specification']['time_end']
             # TODO what to do about groups...
-            sim.addSynapse(stimulus_type, spec, [], prob, time_start, time_end)
+            sim.addStimulus(stimulus_type, spec, [], prob, time_start, time_end)
         for report in reports:
             # TODO: Validate report spec
             pass
             # Get report type from the model
-            report_type = report['specification']['report_type']['type']
+            report_type = report['specification']['report_type']
             report_target = stimulus['specification']['report_target']
             probability = stimulus['specification']['probability']
             time_start = stimulus['specification']['time_start']
@@ -203,9 +256,31 @@ class ModelHelper(object):
             # TODO what to do about targets...
             sim.addReport([], target_type, report_type, probability,
                           time_start, time_end)
-        # time for the groups :(
-        for group in groups:
-            pass
+        return errors
+
+
+    @classmethod
+    def convert_neuron_type(cls, neuron_type):
+        if neuron_type == 'izh_neuron':
+            return 'izhikevich'
+        else:
+            return neuron_type
+
+    @classmethod
+    def convert_unicode_ascii(cls, spec):
+        for param, value in spec.iteritems():
+            v = value
+            del spec[param]
+            spec[param.encode('ascii','ignore')] = value
+
+    @classmethod
+    def process_normal_uniform_parameters(cls, spec):
+        for param, value in spec.iteritems():
+            if type(value) is dict:
+                if value['type'] == 'normal':
+                    spec[param] = ncs.Normal(value['mean'], value['stdev'])
+                if value['type'] == 'uniform':
+                    spec[param] = ncs.Uniform(value['min'], value['max'])
 
 
     def create_ncs_normal(cls, params):
